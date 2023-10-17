e29fac75c2c41d0341c745eadaaf162f9c10b9f2
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-10-17 15:45:09 +0530

    config changes for proxy

--- proxy.config.js
+++ proxy.config.js
@@ -1,16 +1,6 @@
 const PROXY_CONFIG = [
     {
-        context: ["/assets/public/content/*"],
-        "target": "https://sunbirdsaaspublic.blob.core.windows.net/content/content/",
-        "secure": false,
-        "changeOrigin": true,
-        "logLevel": "debug",
-        "pathRewrite": {
-            "^/assets/public/content": ""
-        }
-    },
-    {
-        context: ["/assets/content-player/assets/public/content/*"],
+        context: ["/assets/content-player/assets/public/content"],
         "target": "https://sunbirdsaaspublic.blob.core.windows.net/content/content/",
         "secure": false,
         "changeOrigin": true,
@@ -20,12 +10,20 @@ const PROXY_CONFIG = [
         }
     },
     {
+        context: ["/assets/public/content/*"],
+        "target": "https://sunbirdsaaspublic.blob.core.windows.net/content/content/",
+        "secure": false,
+        "changeOrigin": true,
+        "logLevel": "debug",
+    },
+
+    {
         context: [
             "/action",
             "/content",
             "/api",
             "/assets",
-            
+
         ],
         "target": "https://sunbirdsaas.com/",
         "secure": false,
