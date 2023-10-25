bc8d0a0ce1f98e928b2854dc88599b78924a2445
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-10-25 16:02:07 +0530

    added cards in crete-pitara

--- src/app/components/create-pitara/create-pitara.component.ts
+++ src/app/components/create-pitara/create-pitara.component.ts
@@ -7,12 +7,13 @@ import { UtilService } from 'src/app/services/utils.service';
   styleUrls: ['./create-pitara.component.scss']
 })
 export class CreatePitaraComponent implements OnInit {
+  data: any;
 
   constructor(public utils: UtilService) { }
 
   ngOnInit(): void {
     this.utils.setTitle('Create Your Own Pitara');
-
+    this.data = JSON.parse(localStorage.getItem('contents'));
   }
 
 }
